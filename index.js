ff062559b2a0e22ae380b2bd77bf95c3e1d89ca1
venkata122528 <venkata122528@gmail.com>
2024-10-25 12:15:57 +0000

    admitSpot Backend

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
+
+let db = null;
+const dbPath = path.join(__dirname, "flowAI.db");
+const app = express();
+
+const initializeDataBaseServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server is running on port number 3000");
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+app.use(express.json());
+
+initializeDataBaseServer();
+
+app.post("/contact-management/", async (request, response) => {
+  const getBody = request.body;
+  console.log(getBody);
+  const { name, email, phoneNumber, address, timezone } = getBody;
+});
